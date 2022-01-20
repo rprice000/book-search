dd558c3f1cf9ab33cbc704d834efc7be499d07c7
rprice000 <53877937+rprice000@users.noreply.github.com>
2022-01-20 09:53:43 -0600

    setup base code for resolvers.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,17 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+
+
+
+const resolvers = {
+
+
+
+
+
+};
+
+
+
+module.exports = resolvers;
