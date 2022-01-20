a153d7fcab0255acb10998d08da7523e3fa63cbe
rprice000 <53877937+rprice000@users.noreply.github.com>
2022-01-20 09:57:52 -0600

    added query resolver to resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,11 +5,36 @@ const { signToken } = require('../utils/auth');
 
 
 const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select("-__v -password")
+          .populate("books");
 
+        return userData;
+      }
 
-
-
-
+      throw new AuthenticationError("Not logged in");
+    },
+    users: async () => {
+      return User.find()
+        .select("-__v -password")
+        .populate("books");
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("books");
+    },
+    books: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Book.find(params).sort({ createdAt: -1 });
+    },
+    book: async (parent, { _id }) => {
+      return Book.findOne({ _id });
+    },
+  },
 };
 
 
