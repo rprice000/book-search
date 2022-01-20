1d3d1e994c12c2422f3e365ce621ca6b2c0ffd13
rprice000 <53877937+rprice000@users.noreply.github.com>
2022-01-20 13:28:29 -0600

    adeed mutations to mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,88 @@
 import { gql } from '@apollo/client';
+
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            title
+            description
+            authors
+            link
+            image
+         }
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+        token
+        user {
+          _id
+          username
+          email
+          bookCount
+          savedBooks {
+              bookId
+              title
+              description
+              authors
+              link
+              image
+           }
+        }
+      }
+    }
+`;
+
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($id: ID!) {
+    saveBook(bookId: $id) {
+        _id
+        username
+        email
+        savedBooks {
+            bookId
+            authors
+            image
+            description
+            title
+            link
+        }
+    }
+}
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($id: ID!) {
+    removeBook(bookId: $id) {
+        _id
+        username
+        email
+        savedBooks {
+            bookId
+            authors
+            image
+            description
+            title
+            link
+        }
+    }
+}
+`;
+
+
+
+
+
